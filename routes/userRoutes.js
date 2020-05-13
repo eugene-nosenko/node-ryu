57b904c6655b4a3cb4592309d39e95e29dd850b1
eugene-finn <1287175@gmail.com>
2020-05-13 13:11:03 +0300

    nodemon

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,27 @@
-const { Router } = require('express');
-const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const UserService = require("../services/userService");
+const {
+  createUserValid,
+  updateUserValid,
+} = require("../middlewares/user.validation.middleware");
+const { responseMiddleware } = require("../middlewares/response.middleware");
 
 const router = Router();
+router.post(
+  "/",
+  (req, res, next) => {
+    try {
+      // TODO: Implement login action
+      //res.data = data;
+      console.log(req);
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 // TODO: Implement route controllers for user
 
